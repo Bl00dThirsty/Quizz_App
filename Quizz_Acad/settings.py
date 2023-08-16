7058bc5522a20829bc04c2ab901ba503d29b947e
root <yvana2005>
2023-08-16 15:51:01 +0100

    premiere sauvegarde

--- Quizz_Acad/settings.py
+++ Quizz_Acad/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for Quizz_Acad project.
 
@@ -14,7 +16,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -117,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    # Chemin vers le répertoire contenant votre fichier CSS et d'autres fichiers statiques
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

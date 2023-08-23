a1cae46266ea58a7a580f6d20e3c213ec6fc3b93
John Davis <johndavisambassa@gmail.com>
2023-08-23 11:04:15 +0100

    deuxieme commit

--- Quizz_Acad/settings.py
+++ Quizz_Acad/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quizz',
-    'accounts',
+    'app.quizz',
+    'app.accounts',
 ]
 
 MIDDLEWARE = [
